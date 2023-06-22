4fc8807db0dd2877418e3a530f277fe16425500e
Marcelo Rodrigues <marcelosrc@gmail.com>
2023-06-21 21:03:42 -0300

    Estruturação da página de itens em inventário

--- src/components/Inventory.jsx
+++ src/components/Inventory.jsx
@@ -1,13 +1,29 @@
 import React from "react";
 import "../styling/Inventory.scss";
+import axios from "axios";
 
 function Inventory() {
+  const [inventory, setInventory] = React.useState([]);
 
-  return (
-    <div className="inventory__container">
-      <p>Essa página aqui vai ter um scroll de itens de inventário</p>
+  React.useEffect(() => {
+    axios
+      .get("/gm/inventory")
+      .then((res) => {
+        setInventory(res.data.message);
+      })
+      .catch((err) => {
+        alert(err.response.data.message);
+      });
+  }, []);
+
+  const showInventory = inventory.map((item) => (
+    <div className="inventory__item" key={item.id}>
+      <p>{item.item}</p>
+      <p>{item.quantity}</p>
     </div>
-  );
+  ));
+
+  return <div className="inventory__container">{showInventory}</div>;
 }
 
 export default Inventory;
